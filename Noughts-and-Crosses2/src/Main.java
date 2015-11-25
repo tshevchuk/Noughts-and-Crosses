477c5fb6b11043f045a6e689bf505ff171ec142c
Roman <shevromanvk@gmail.com>
2015-11-25 22:24:08 +0200

    Вивести порожнє поле #1

--- Noughts-and-Crosses2/src/Main.java
+++ Noughts-and-Crosses2/src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println(11);
+        System.out.println("   |   |  ");
+        System.out.println("---+---+--- ");
+        System.out.println("   |   | ");
+        System.out.println("---+---+---");
+        System.out.println("   |   | ");
+
     }
 }
