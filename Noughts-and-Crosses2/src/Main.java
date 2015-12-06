eb02b105c8b7421d037be1f4aed2a4245e326bc2
Roman <shevromanvk@gmail.com>
2015-12-06 23:12:59 +0200

    Програма запам’ятовує в якій клітинці яка є введена фігура, і виводить всі старі значення разом із новим значенням #5

--- Noughts-and-Crosses2/src/Main.java
+++ Noughts-and-Crosses2/src/Main.java
@@ -5,129 +5,30 @@ import java.nio.Buffer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        System.out.println("   |   |  ");
-        System.out.println("---+---+--- ");
-        System.out.println("   |   | ");
-        System.out.println("---+---+---");
-        System.out.println("   |   | ");
-        int p = 1;
-        while (p <= 9) {
-            p++;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            int x = Integer.parseInt(reader.readLine());
-            if (x == 1) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println(" x |   | ");
-            } else if (x == 2) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   | x | ");
-            } else if (x == 3) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | x");
-            } else if (x == 4) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println(" x |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (x == 5) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   | x | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (x == 6) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | x");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (x == 7) {
-                System.out.println(" x |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (x == 8) {
-                System.out.println("   | x |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (x == 9) {
-                System.out.println("   |   | x ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            }
-            BufferedReader reader1 = new BufferedReader(new InputStreamReader(System.in));
-            int o = Integer.parseInt(reader1.readLine());
-            if (o == 1) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println(" o |   | ");
-            } else if (o == 2) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   | o | ");
-            } else if (o == 3) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | o");
-            } else if (o == 4) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println(" o |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (o == 5) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   | o | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (o == 6) {
-                System.out.println("   |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | o");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (o == 7) {
-                System.out.println(" o |   |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (o == 8) {
-                System.out.println("   | o |  ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            } else if (o == 9) {
-                System.out.println("   |   | o ");
-                System.out.println("---+---+--- ");
-                System.out.println("   |   | ");
-                System.out.println("---+---+---");
-                System.out.println("   |   | ");
-            }
-        }
+        char field[] = new char[9];
+        for (int i = 0; i < 9; i++) {
+            field[i] = ' ';
 
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (; ; ) {
+
+            int x = Integer.parseInt(reader.readLine());
+            field[x - 1] = 'X';
+            printField(field);
+
+            int о = Integer.parseInt(reader.readLine());
+            field[о - 1] = 'О';
+            printField(field);
+        }
+    }
+
+
+    public static void printField(char[] field) {
+        System.out.println(" " + field[6] + " | " + field[7] + " | " + field[8]);
+        System.out.println("---+---+--- ");
+        System.out.println(" " + field[3] + " | " + field[4] + " | " + field[5]);
+        System.out.println("---+---+---");
+        System.out.println(" " + field[0] + " | " + field[1] + " | " + field[2]);
     }
 }
